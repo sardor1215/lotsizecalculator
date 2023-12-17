e3e41ab65d3778b10399967ffe18167878eb6dbf
Sardor1215 <clay.hackers30@gmail.com>
2023-12-17 15:58:47 +0200

    v-3.1

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer items-center w-full text-center text-white py-2 bg-indigo-500 text-neutral-content">
+      <aside className="items-center grid-flow-col">
+        <p>Sardors © 2023 - All right reserved</p>
+      </aside>
+    </footer>
+  );
+}
--- components/Navbar.js
+++ components/Navbar.js
@@ -8,8 +8,8 @@ export default function Navbar({ fixed }) {
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="#pablo"
+              className="text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              href=""
             >
               FX Calculator
             </a>
